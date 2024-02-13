82d46147e947154dad543e387e13010f578607fc
Angel205 <angelina.pinchuk.205@gmail.com>
2024-02-13 09:37:26 +0300

    add search

--- WebPage/views.py
+++ WebPage/views.py
@@ -29,6 +29,10 @@ class CarsListView(ListView):
 
         return context
 
+
+    
+
+
 def refresh_bd(request):
     CarModel.objects.filter(date__lt=datetime.now()).delete()
     copart = CopartParse(url='https://www.copart.com/lotSearchResults?free=false&searchCriteria=%7B%22query%22:%5B%22*%22%5D,%22filter%22:%7B%22VEHT%22:%5B%22vehicle_type_code:VEHTYPE_V%22%5D,%22ODM%22:%5B%22odometer_reading_received:%5B0%20TO%209999999%5D%22%5D,%22YEAR%22:%5B%22lot_year:%5B2019%20TO%202024%5D%22%5D%7D,%22searchName%22:%22%22,%22watchListOnly%22:false,%22freeFormSearch%22:false%7D%20&displayStr=Search%20vehicles&from=%2FvehicleFinder', count_page=1)
